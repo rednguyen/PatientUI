771626fad3ffa5d88dc5fe6200da214dadf2bede
devisureshh <devisuresh.pyl@gmail.com>
2021-11-22 08:13:04 -0800

    fifth commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
    {path: '', component: HomeComponent, pathMatch: 'full'},
    {path: 'about', component: AboutComponent},
-   {path: 'form', component: FormComponent}
+   {path: 'form', component: FormComponent},
+   {path: 'contact', component: ContactComponent},
+
 ];
 
 @NgModule({
