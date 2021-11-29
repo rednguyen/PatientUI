f7c862233d8d4e18b0279aedddd94e1049947a75
oddtomas <oddthomas@csu.fullerton.edu>
2021-11-28 16:25:45 -0800

    added a sidebar nav

---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
+})
+export class SideNavComponent  {
+  opened = false;
+
+  toggleSidebar() {
+    this.opened = !this.opened;
+    
+  }
+
+}
